99dc70db8cfb2e089c1fae581f5657a3e5790784
BlckBob <sboban001@gmail.com>
2024-11-16 20:24:36 +0100

    Footer refactoring

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Footer = () => (
   <footer className="footer_card hover:bg-primary/100">
-    <p className="sm:text-16-footer text-[2vw] font-normal uppercase">
+    <p className="sm:text-16-footer flex items-start justify-center p-0 font-work-sans text-[2.5vw] font-semibold uppercase max-[376px]:text-9px">
       &copy; {new Date().getFullYear()} Boban Stojanovic &nbsp;&nbsp;
     </p>
     <Image
@@ -9,18 +9,18 @@ const Footer = () => (
       alt="logo"
       width={32}
       height={32}
-      className="-mt-2 opacity-100 transition delay-150 ease-in-out hover:scale-125"
+      className="-mx-1 -mt-2 mb-0 p-0 opacity-100 transition delay-150 ease-in-out hover:scale-125"
     />
-    <p className="sm:text-16-footer text-normal text-[2vw] uppercase">
+    <p className="sm:text-16-footer m-0 flex items-start justify-center p-0 font-work-sans text-[2.5vw] font-semibold uppercase max-[359px]:flex-wrap max-[376px]:text-9px">
       {" "}
-      &nbsp; Home Inc. All rights reserved.&nbsp;&nbsp;
+      &nbsp;Home Inc. All rights reserved&nbsp;
     </p>
     <Image
       src="/black-sb-01.png"
       alt="logo"
       width={32}
       height={32}
-      className="-mt-2 opacity-100 transition delay-100 ease-out hover:scale-125"
+      className="-mr-2 -mt-2 mb-0 ml-0 p-0 opacity-100 transition delay-100 ease-out hover:scale-125"
     />
   </footer>
 );
